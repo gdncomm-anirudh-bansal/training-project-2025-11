f6a674d663c117dfcfb6c70c43f2b302f44ca2a7
Anirudh Bansal <anirudh.bansal@gdn-commerce.com>
2025-12-04 17:02:00 +0530

    Re setup the repop

--- product/src/main/java/com/project/Search/Repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.Search.Repository;
-
-import com.project.Search.Entity.Category;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryRepository extends MongoRepository<Category,String>{
-}
--- product/src/main/java/com/project/Search/Service/CategoryService.java
+++
@@ -1,4 +0,0 @@
-package com.project.Search.Service;
-
-public interface CategoryService {
-}
